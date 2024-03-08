8109b8d92bb373befbc00bdf5619e769e192f75d
Guilherme Suganame <gsuganame@gmail.com>
2024-03-08 14:53:49 -0300

    setup: spring-data-jpa + h2database configuration

---
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/SpringBootExpertApplication.java
@@ -0,0 +1,13 @@
+package com.suganame.springbootexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExpertApplication.class, args);
+	}
+
+}
