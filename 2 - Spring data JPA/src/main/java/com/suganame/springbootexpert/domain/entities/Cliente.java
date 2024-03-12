14597a224b8b7075925681aec6472dae33cfce17
Guilherme Suganame <gsuganame@gmail.com>
2024-03-12 13:16:32 -0300

    aula 21 - mapeando a entidade Cliente para JPA

--- 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Cliente.java
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Cliente.java
@@ -1,5 +1,11 @@
 package com.suganame.springbootexpert.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +15,15 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity
+@Table(name = "CLIENTE")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "nome", length = 100)
     private String nome;
 
 }
