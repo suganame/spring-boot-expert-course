87ffd20fe62ff77048e6799fa498b73c6bf3f213
Guilherme Suganame <gsuganame@gmail.com>
2024-03-12 15:55:10 -0300

    aula 28 - mapeando a entidade produto

--- 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Produto.java
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Produto.java
@@ -2,11 +2,26 @@ package com.suganame.springbootexpert.domain.entities;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "produto")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao")
     private String decricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 }
