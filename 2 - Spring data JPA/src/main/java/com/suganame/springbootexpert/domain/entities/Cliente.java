4fd8ece8f0274e82c45d1d14c035b6736f320102
Guilherme Suganame <gsuganame@gmail.com>
2024-03-12 16:01:34 -0300

    aula 29 - mapeando a entidade pedido

--- 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Cliente.java
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Cliente.java
@@ -1,10 +1,14 @@
 package com.suganame.springbootexpert.domain.entities;
 
+import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,5 +29,8 @@ public class Cliente {
 
     @Column(name = "nome", length = 100)
     private String nome;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos;
 
 }
--- 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Pedido.java
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Pedido.java
@@ -3,12 +3,31 @@ package com.suganame.springbootexpert.domain.entities;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "pedido")
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
+    @Column(name = "data_pedido")
     private LocalDate dataPedido;
+
+    @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
 }
