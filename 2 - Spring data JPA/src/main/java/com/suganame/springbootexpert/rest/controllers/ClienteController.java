dbea614b360ee67c447a69e1693fba3843d4c690
Guilherme Suganame <gsuganame@gmail.com>
2024-03-13 10:35:47 -0300

    aula 36 - criando controller de cliente

---
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/rest/controllers/ClienteController.java
@@ -0,0 +1,19 @@
+package com.suganame.springbootexpert.rest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloClientes(@PathVariable("nome") String nomeCliente) {
+        return String.format("Hello %s", nomeCliente);
+    }
+
+}
