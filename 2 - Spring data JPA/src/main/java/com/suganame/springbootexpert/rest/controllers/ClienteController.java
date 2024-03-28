b9c87a9ddd768723608ac1a26df0b9cfe8080d88
Guilherme Suganame <gsuganame@gmail.com>
2024-03-28 10:45:58 -0300

    refactor: remove unecessary imports

--- 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/rest/controllers/ClienteController.java
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/rest/controllers/ClienteController.java
@@ -1,13 +1,11 @@
 package com.suganame.springbootexpert.rest.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
