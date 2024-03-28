1d7c9827e5acb31ca26e5d97a16863db93360b3c
Guilherme Suganame <gsuganame@gmail.com>
2024-03-28 10:40:55 -0300

    fix: some bugs

--- 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Produto.java
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/domain/entities/Produto.java
@@ -20,7 +20,7 @@ public class Produto {
     private Integer id;
 
     @Column(name = "descricao")
-    private String decricao;
+    private String descricao;
 
     @Column(name = "preco_unitario")
     private BigDecimal preco;
--- 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/rest/controllers/ProdutoController.java
+++ 2 - Spring data JPA/src/main/java/com/suganame/springbootexpert/rest/controllers/ProdutoController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.suganame.springbootexpert.domain.entities.Cliente;
 import com.suganame.springbootexpert.domain.entities.Produto;
 import com.suganame.springbootexpert.domain.repositories.Produtos;
 
