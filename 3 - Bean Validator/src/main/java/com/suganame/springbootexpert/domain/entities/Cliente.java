2501cc187a802d4955b88829577c514e100d5569
Guilherme Suganame <gsuganame@gmail.com>
2024-04-11 19:30:47 -0300

    aula 61 - Validando a entidade cliente

--- 3 - Bean Validator/src/main/java/com/suganame/springbootexpert/domain/entities/Cliente.java
+++ 3 - Bean Validator/src/main/java/com/suganame/springbootexpert/domain/entities/Cliente.java
@@ -3,6 +3,8 @@ package com.suganame.springbootexpert.domain.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
@@ -34,6 +36,8 @@ public class Cliente {
     private String nome;
 
     @Column(name = "cpf", length = 11)
+    @NotEmpty(message = "Campo CPF é obrigatório.")
+    @CPF(message = "Informe um CPF válido.")
     private String cpf;
 
     @JsonIgnore
